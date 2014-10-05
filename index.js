bfbf7aa1171507132fb7d4e0aa2670e7bbdd09fe
Javier Carrillo <stringparser@gmail.com>
2014-10-05 20:53:34 +0200

    leave checks to v8-callsites

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 var callsites = require('v8-callsites');
 
 function callersPath(frames, origin){
-  var stack = callsites(frames, origin || callersPath);
+
+  var stack = callsites(frames || 2, origin || callersPath);
   return stack[stack.length-1].getFileName();
 }
 
