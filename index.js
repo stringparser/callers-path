e3304b310cb9f7e4e8159face5ce9183a8f5b845
Javier Carrillo <stringparser@gmail.com>
2014-10-05 20:52:18 +0200

    leave checks to v8-callsites

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict';
 
-var type = require('utils-type');
 var callsites = require('v8-callsites');
 
 function callersPath(frames, origin){
-
-  frames = (type(frames).integer || 0) + 2;
   var stack = callsites(frames, origin || callersPath);
   return stack[stack.length-1].getFileName();
 }
--- test/fileName.js
+++ test/fileName.js
@@ -4,8 +4,8 @@ var should = require('should');
 
 module.exports = function(callersPath){
 
-  it('should return caller\'s path', function(){
-    var callerPath = callersPath();
+  it('should return caller\'s path', function origin(){
+    var callerPath = callersPath(origin);
 
     should(callerPath).match(/node_modules.mocha/);
   });
