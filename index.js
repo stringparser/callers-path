4237a7319a04e2c16baf06e3ae92116887b0943f
Javier Carrillo <stringparser@gmail.com>
2014-10-08 21:57:45 +0200

    no defaults for the frame

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var callsites = require('v8-callsites');
 
 function callersPath(frames, origin){
 
-  var stack = callsites(frames || 2, origin || callersPath);
+  var stack = callsites(frames, origin || callersPath);
   return stack[stack.length-1].getFileName();
 }
 
