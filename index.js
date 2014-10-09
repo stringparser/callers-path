61a51699ec179c355b6ec9a95b3a69b23d9957b3
Javier Carrillo <stringparser@gmail.com>
2014-10-09 15:32:18 +0200

    fix problem for only one argument

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var callsites = require('v8-callsites');
 
 function callersPath(frames, origin){
 
-  var stack = callsites(frames || 2, frames || origin || callersPath);
+  origin = typeof frames === 'function' ? frames : origin;
+  var stack = callsites(frames || 2, origin || callersPath);
   return stack[stack.length-1].getFileName();
 }
 
