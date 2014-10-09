5ac415e3febe3612a4f566c8ef3dd59bc894e392
Javier Carrillo <stringparser@gmail.com>
2014-10-09 14:20:41 +0200

    fixed problem of defaults

--- test/fileName.js
+++ test/fileName.js
@@ -4,7 +4,8 @@ var should = require('should');
 
 module.exports = function(callersPath){
 
-  it('should return caller\'s path', function origin(){
+  console.log(callersPath());
+  it('should return caller\'s path', function (){
     var callerPath = callersPath();
 
     should(callerPath).match(/node_modules.mocha/);
