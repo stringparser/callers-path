9eb22cd86f0e1581516850aaecea315324e450aa
Javier Carrillo <stringparser@gmail.com>
2014-10-15 19:24:23 +0200

    ups

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var callsites = require('v8-callsites');
 function callersPath(origin){
 
   var stack = callsites(origin ? 1 : 2, origin || callersPath);
-  return stack[1].getFileName();
+  return stack[stack.length-1].getFileName();
 }
 
 module.exports = callersPath;
