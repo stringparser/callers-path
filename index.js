9bfa57bf28c7e55a28831a55ec20abc9e1087071
Javier Carrillo <stringparser@gmail.com>
2014-10-15 16:38:26 +0200

    tweak

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var callsites = require('v8-callsites');
 
 function callersPath(frames, origin){
 
-  origin = typeof frames === 'function' ? frames : origin;
-  var stack = callsites(frames || 2, origin || callersPath);
+  var stack = callsites(frames || 2, origin || frames || callersPath);
   return stack[stack.length-1].getFileName();
 }
 
