6bdeb8ede9bd1dc61dadcc1763bc2ebcc43597b9
Javier Carrillo <stringparser@gmail.com>
2014-10-15 16:48:22 +0200

    simpler

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 var callsites = require('v8-callsites');
 
-function callersPath(frames, origin){
+function callersPath(origin){
 
-  var stack = callsites(frames || 2, origin || frames || callersPath);
-  return stack[stack.length-1].getFileName();
+  var stack = callsites(2, origin || callersPath);
+  return stack[1].getFileName();
 }
 
 module.exports = callersPath;
