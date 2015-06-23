32be63aaec553e5d322bec8d36de952b9377a254
Javier Carrillo <stringparser@gmail.com>
2015-06-23 12:57:57 +0200

    dev: minor improvements, remove jshint-stylish dep

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ var callsites = require('v8-callsites');
 
 function callersPath(origin){
   if(typeof origin !== 'function'){ origin = null; }
-  var stack = callsites( origin ? 1 : 2, origin || callersPath);
+
+  var stack = callsites(
+    origin && 1 || 2,
+    origin || callersPath
+  );
+
   return stack[stack.length-1].getFileName();
 }
 
